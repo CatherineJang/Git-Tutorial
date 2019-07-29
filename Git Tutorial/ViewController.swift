309a8f031f7c2ae7de38ebae742a5249b81a7617
Catherine Jang <catherinehjang@gmail.com>
2019-07-29 07:48:27 -0700

    Initial Commit

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by Catherine Jang on 7/29/19.
+//  Copyright © 2019 Catherine Jang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
